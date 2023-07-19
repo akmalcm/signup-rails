b5e6dcf34dc5f1d7a9da7d41cba4e74fdc594f8a
Akmal <49616983+akmalcm@users.noreply.github.com>
2023-07-19 20:49:14 +0800

    Feature/upgrade rails (#7)
    * upgrade rails v5.2.8
    
    * upgrade rails v6.0.6
    
    * upgrade rails v6.1.7
    
    * upgrade ruby v3.2.2
    
    * upgrade rails v7.0.6
    
    * update github action
    
    * update rubocop todo

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 7.0
     config.time_zone = 'Asia/Kuala_Lumpur'
 
     # Settings in config/environments/* take precedence over those specified here.
